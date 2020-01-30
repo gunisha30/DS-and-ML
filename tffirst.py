0f369e65b2cb8abd3f4981221c551af6d78ab380
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-01-30 16:15:34 +0530

    Update tffirst.py

--- tffirst.py
+++ tffirst.py
@@ -1,71 +1,22 @@
 
 
 import tensorflow as tf
-
-
 from tensorflow import keras
-
-
-
-
-
 import numpy as np
-
-
-
 import matplotlib.pyplot as plt
-
-
-
-
 d=keras.datasets.fashion_mnist
-
-
-
-
-
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 (train_images,train_labels),(test_images,test_labels)=d.load_data()
 train_images = train_images / 255.0
-
 test_images = test_images / 255.0
-
-
-
-
-
-
 model=keras.Sequential(
     [keras.layers.Flatten(input_shape=(28,28)),keras.layers.Dense(128,activation="relu"),keras.layers.Dense(10,activation="softmax")])
-
-
-
-
-
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-
-
-
-
-
 model.fit(train_images, train_labels, epochs=5)
-
-
-
-
-
 predictions = model.predict(test_images) 
-
-
-
-
-
-
-
-
 for i in range(5):
     plt.grid(False)
     plt.imshow(test_images[i],cmap=plt.cm.binary)
